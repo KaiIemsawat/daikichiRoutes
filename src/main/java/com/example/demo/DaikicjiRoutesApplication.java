7371a0e2ce761ce8659f4d930456b345c9181a8b
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-03-20 19:28:48 -0400

    first commit

---
+++ src/main/java/com/example/demo/DaikicjiRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikicjiRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikicjiRoutesApplication.class, args);
+	}
+
+}
